8d77b770471b88b17fda44797ba3cf211ca5857c
Дон <dsvinkabot@gmail.com>
2021-09-08 00:42:47 +0300

    TextMesh Pro?
    Он опять что-то делает с шрифтами...

---
+++ Assets/Code/SaveData/Data/LocationSaveData.cs
@@ -0,0 +1,87 @@
+using System;
+using Code.Providers;
+using Code.Utils.Extensions;
+using UnityEngine;
+
+namespace Code.SaveData.Data
+{
+    [Serializable]
+    internal struct Modificator
+    {
+        public bool IsActive;
+    }
+
+    [Serializable]
+    internal struct Target
+    {
+        public bool IsActive;
+        public float Health;
+    }
+    
+    [Serializable]
+    internal struct Wall
+    {
+        public bool IsActive;
+        public Target[] Targets;
+    }
+    
+    [Serializable]
+    internal struct Pickup
+    {
+        public bool IsActive;
+    }
+    
+    [Serializable]
+    internal sealed class LocationSaveData
+    {
+        public string LocationNameID;
+        public Modificator[] Modificators;
+        public Pickup[] Pickups;
+        public Wall[] Walls;
+
+        public void ModificatorsSet(ModificatorProvider[] modificatorProviders)
+        {
+            Modificators = new Modificator[modificatorProviders.Length];
+            for (var index = 0; index < modificatorProviders.Length; index++)
+            {
+                var modificatorProvider = modificatorProviders[index];
+                Modificators[index].IsActive = modificatorProvider.Parent.activeSelf;
+            }
+        }
+        
+        public void PickupsSet(PickupProvider[] pickupProviders)
+        {
+            Pickups = new Pickup[pickupProviders.Length];
+            for (var index = 0; index < pickupProviders.Length; index++)
+            {
+                var pickupProvider = pickupProviders[index];
+                Pickups[index].IsActive = pickupProvider.Parent.activeSelf;
+            }
+        }
+        
+        // TODO: Добавить сохранение мишеней на стене. (А лучше сделать нормальных противников - автономные "ДОТы")
+        public void WallsSet(WallProvider[] wallProviders)
+        {
+            Walls = new Wall[wallProviders.Length];
+            for (var index = 0; index < wallProviders.Length; index++)
+            {
+                var wallProvider = wallProviders[index];
+                var wallTargets = wallProvider.TargetProviders;
+                var newWallTargets = new Target[wallProvider.TargetProviders.Length];
+                
+                for (var i = 0; i < wallTargets.Length; i++)
+                {
+                    var target = wallTargets[i];
+                    newWallTargets[i] = new Target
+                    {
+                        Health = target.Health,
+                        IsActive = target.gameObject.activeSelf,
+                    };
+                }
+
+                Walls[index].IsActive = wallProvider.gameObject.activeSelf;
+                Walls[index].Targets = newWallTargets;
+            }
+        }
+    }
+}
